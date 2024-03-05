e41fb0f9dd34d4d8e051b71a9059da2cde2b4cd5
Glowac45 <lukaszglowac@interia.pl>
2024-03-05 16:55:32 +0100

    Ćw 3 5.03

--- filmy/urls.py
+++ filmy/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from filmy.views import wszystkie, szczegoly, nowy, edycja, usun
+from django.contrib import admin
 
 urlpatterns = [
     path('wszystkie/', wszystkie),
     path('szczegoly/<int:film_id>/', szczegoly),
     path('nowy/', nowy),
     path('edycja/<int:film_id>/', edycja),
-    path('usun/<int:film_id>/', usun)
+    path('usun/<int:film_id>/', usun),
+    path('admin/', admin.site.urls)
 ]
