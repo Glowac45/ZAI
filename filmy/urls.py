26bc58289cdcecc94670218bc5b469e68c334a6d
Glowac45 <lukaszglowac@interia.pl>
2024-03-05 16:36:04 +0100

    Ćw 2 27.02

--- filmy/urls.py
+++ filmy/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from filmy.views import wszystkie, szczegoly
+from filmy.views import wszystkie, szczegoly, nowy
 
 urlpatterns = [
     path('wszystkie/', wszystkie),
-    path('szczegoly/<int:film_id>/', szczegoly)
+    path('szczegoly/<int:film_id>/', szczegoly),
+    path('nowy/', nowy)
 ]
